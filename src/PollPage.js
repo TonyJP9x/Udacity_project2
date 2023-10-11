46d195785e0a92df97eeb3b6d1f6c965ae7b0131
thuan <phamdinhthuan1995@gmail.com>
2023-10-12 00:53:14 +0700

    update

--- src/PollPage.js
+++ src/PollPage.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import NavBar from './NavBar';
-import { useDispatch, useSelector } from 'react-redux';
-import { Link, useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import DetailPage from './DetailPage';
 
 
